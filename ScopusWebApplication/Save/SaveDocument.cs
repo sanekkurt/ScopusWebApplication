4d0e47517567be86d3c1fa3f32bdb5ef829fdc00
Александр <@DESKTOP-4KOL7E1>
2020-02-19 21:16:48 +0500

    Доработан до полного правильного отображения шаблон ГОСТ 7.1-2003 + ГОСТ Р 7.0.5-2008

--- ScopusWebApplication/Save/SaveDocument.cs
+++ ScopusWebApplication/Save/SaveDocument.cs
@@ -15,67 +15,90 @@ namespace ScopusWebApplication.Save
             Word.Document wordDoc = wordApp.Documents.Add();
             var wordParag = wordDoc.Paragraphs.Add();
 
-            //Вывод всех авторов в одну строку с верным форматированием
-            string authors = "";
-            int lenAuthorsArray = articles[0].authors.Length;
-            for (int i = 0; i< lenAuthorsArray; i++)
+            for (int j = 0; j < articles.Count; j++)
             {
-                if(i != lenAuthorsArray - 1)
+                //Вывод всех авторов в одну строку с верным форматированием
+                string authors = "";
+                int lenAuthorsArray = articles[j].authors.Length;
+                for (int i = 0; i < lenAuthorsArray; i++)
                 {
-                    authors = authors.Insert(authors.Length, articles[0].authors[i].Surname + " " + articles[0].authors[i].CeInitials + ", ");
+                    if (i != lenAuthorsArray - 1)
+                    {
+                        authors = authors.Insert(authors.Length, articles[j].authors[i].Surname + " " + articles[j].authors[i].CeInitials + ", ");
+                    }
+                    else
+                    {
+                        authors = authors.Insert(authors.Length, articles[j].authors[i].Surname + " " + articles[j].authors[i].CeInitials + " ");
+                    }
                 }
-                else
-                {
-                    authors = authors.Insert(authors.Length, articles[0].authors[i].Surname + " " + articles[0].authors[i].CeInitials + " ");
-                }                
-            }
 
-            //Вывод всех авторов в одну строку, только наоборот и с дополнительными косыми знаками
-            string authorsReverse = "/ ";
-            for (int i = 0; i < lenAuthorsArray; i++)
-            {                
-                if (i != lenAuthorsArray - 1)
+                //Вывод всех авторов в одну строку, только наоборот и с дополнительными косыми знаками
+                string authorsReverse = "/ ";
+                for (int i = 0; i < lenAuthorsArray; i++)
                 {
-                    authorsReverse = authorsReverse.Insert(authorsReverse.Length, articles[0].authors[i].CeInitials + " " + articles[0].authors[i].Surname + ", ");
+                    if (i != lenAuthorsArray - 1)
+                    {
+                        authorsReverse = authorsReverse.Insert(authorsReverse.Length, articles[j].authors[i].CeInitials + " " + articles[j].authors[i].Surname + ", ");
+                    }
+                    else
+                    {
+                        authorsReverse = authorsReverse.Insert(authorsReverse.Length, articles[j].authors[i].CeInitials + " " + articles[j].authors[i].Surname + " // ");
+                    }
                 }
-                else
+
+                //Вывод года
+                string formatYear = " — . — ";
+                formatYear = formatYear.Insert(3, articles[j].year.Remove(4));
+
+                //Вывод VOLUME
+                string formatVolume = "";
+                if (articles[j].volume != null)
                 {
-                    authorsReverse = authorsReverse.Insert(authorsReverse.Length, articles[0].authors[i].CeInitials + " " + articles[0].authors[i].Surname + " // ");
+                    if (articles[j].number == null)
+                    {
+                        formatVolume = "Vol. .";
+                    }
+                    else
+                    {
+                        formatVolume = "Vol. , ";
+                    }
+
+                    formatVolume = formatVolume.Insert(5, articles[j].volume);
                 }
+
+                //Вывод номера журнала
+                string formatNumber = "";
+                if (articles[j].number != null)
+                {
+                    formatNumber = "№  .";
+                    formatNumber = formatNumber.Insert(2, articles[j].number);
+                }
+
+                //Вывод количества страниц
+                string formatPages = "";
+                if (articles[j].pages != null)
+                {
+                    if (articles[j].number == null && articles[j].volume == null)
+                    {
+                        formatPages = "P. .";
+                        formatPages = formatPages.Insert(3, articles[j].pages);
+                    }
+                    else
+                    {
+                        formatPages = " — P. .";
+                        formatPages = formatPages.Insert(6, articles[j].pages);
+                    }
+                    if (formatPages.IndexOf("-") != -1)
+                    {
+                        formatPages = formatPages.Replace("-", "—");
+                    }
+                }
+
+                wordParag.Range.Font.Name = "Times New Roman";
+                wordParag.Range.Font.Size = 14;
+                wordParag.Range.Text = authors + articles[j].title + " " + authorsReverse + articles[j].journal + "." + formatYear + formatVolume + formatNumber + formatPages;
+                wordDoc.Paragraphs.Add();
             }
-
-            //Вывод года
-            string formatYear = " — . — ";            
-            formatYear = formatYear.Insert(3, articles[0].year.Remove(4));
-
-            //Вывод VOLUME
-            string formatVolume = "";
-            if (articles[0].volume!=null)
-            {
-                formatVolume = "Vol. , ";
-                formatVolume = formatVolume.Insert(5, articles[0].volume);
-            }
-
-            //Вывод номера журнала
-            string formatNumber = "";
-            if (articles[0].number != null)
-            {
-                formatNumber = "№  .";
-                formatNumber = formatNumber.Insert(2, articles[0].number);
-            }
-
-            //Вывод количества страниц
-            string formatPages = "";
-            if (articles[0].pages != null)
-            {
-                formatPages = " — P. .";
-                formatPages = formatPages.Insert(6, articles[0].pages);
-            }
-
-            wordParag.Range.Font.Name = "Times New Roman";
-            wordParag.Range.Font.Size = 14;
-            wordParag.Range.Text = authors + articles[0].title + " " + authorsReverse + articles[0].journal + "." + formatYear + formatVolume + formatNumber + formatPages;
-            
 
             wordDoc.SaveAs("D:\\test.doc");
             wordApp.ActiveDocument.Close();
