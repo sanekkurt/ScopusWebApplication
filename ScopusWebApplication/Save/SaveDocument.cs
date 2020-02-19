8fad5199a7eb8e1f85b379b40839270f8c227bef
Александр <@DESKTOP-4KOL7E1>
2020-02-19 21:44:58 +0500

    Добавлен новый шаблон VAK (Высшей аттестационной комиссии)

--- ScopusWebApplication/Save/SaveDocument.cs
+++ ScopusWebApplication/Save/SaveDocument.cs
@@ -104,6 +104,90 @@ namespace ScopusWebApplication.Save
             wordApp.ActiveDocument.Close();
             wordApp.Quit();
         }
+
+        public void vak(List<Article> articles)
+        {
+            var wordApp = new Word.Application();
+            wordApp.Visible = false;
+
+            Word.Document wordDoc = wordApp.Documents.Add();
+            var wordParag = wordDoc.Paragraphs.Add();
+
+            for (int j = 0; j < articles.Count; j++)
+            {
+                //Вывод всех авторов в одну строку с верным форматированием
+                string authors = "";
+                int lenAuthorsArray = articles[j].authors.Length;
+                for (int i = 0; i < lenAuthorsArray; i++)
+                {
+                    if (i != lenAuthorsArray - 1)
+                    {
+                        authors = authors.Insert(authors.Length, articles[j].authors[i].Surname + " " + articles[j].authors[i].CeInitials + ", ");
+                    }
+                    else
+                    {
+                        authors = authors.Insert(authors.Length, articles[j].authors[i].Surname + " " + articles[j].authors[i].CeInitials);
+                    }
+                }
+
+
+
+                //Вывод года
+                string formatJournal = articles[j].journal;
+                if (articles[j].year != null)
+                {
+
+                    formatJournal = formatJournal + ", " + articles[j].year.Remove(4) + ".";
+                }
+                else
+                {
+                    formatJournal = formatJournal + ".";
+                }
+
+
+                //Вывод VOLUME
+                string formatVolume = "";
+                if (articles[j].volume != null)
+                {
+
+                    formatVolume = "Vol. . ";
+
+                    formatVolume = formatVolume.Insert(5, articles[j].volume);
+                }
+
+                //Вывод номера журнала
+                string formatNumber = "";
+                if (articles[j].number != null)
+                {
+                    formatNumber = "Is. . ";
+                    formatNumber = formatNumber.Insert(4, articles[j].number);
+                }
+
+                //Вывод количества страниц
+                string formatPages = "";
+                if (articles[j].pages != null)
+                {
+                    formatPages = "Pp. .";
+                    formatPages = formatPages.Insert(4, articles[j].pages);
+
+                    if (formatPages.IndexOf("-") != -1)
+                    {
+                        formatPages = formatPages.Replace("-", "–");
+                    }
+                }
+
+                wordParag.Range.Font.Name = "Times New Roman";
+                wordParag.Range.Font.Size = 14;
+                wordParag.Range.Text = authors + "et. al. " + articles[j].title + " // " + formatJournal + " " + formatVolume + formatNumber + formatPages;
+                wordDoc.Paragraphs.Add();
+            }
+
+
+            wordDoc.SaveAs("D://test.doc");
+            //wordDoc.SaveAs(AppDomain.CurrentDomain.BaseDirectory + @"\" + "test.doc");
+            wordApp.ActiveDocument.Close();
+            wordApp.Quit();
+        }
     }
         
 }
